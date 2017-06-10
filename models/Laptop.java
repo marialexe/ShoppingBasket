c150b1add2c5667295fa795b6a1e91eac298b9e4
Marilena Alexe <marilena.alexe@gmail.com>
2017-06-10 23:21:05 +0100

    Complete the TDD tests in LaptopTest class for the abstract class Item and child Laptop class

--- models/Laptop.java
+++ models/Laptop.java
@@ -6,7 +6,7 @@ public class Laptop extends Item {
 
   private int screenSize;
 
-  public Laptop(String category, String type, Double price, String make, String model) {
+  public Laptop(String category, String type, Double price, String make, String model, int screenSize) {
     super(category,type,price,make,model);
     this.screenSize = screenSize;
   }
--- specs/LaptopTest.java
+++ specs/LaptopTest.java
@@ -13,12 +13,41 @@ public class LaptopTest {
 
   @Before
   public void before(){
-    laptop = new Laptop("Technology","Laptop", 800.00, "Macbook", "Pro");
+    laptop = new Laptop("Technology","Laptop", 800.00, "Macbook", "Pro", 17);
   }
 
   @Test
   public void doesItemExist() {
     assertThat(laptop, instanceOf(Laptop.class));
   }
-  
+
+  @Test
+  public void canGetCategory() {
+    assertEquals("Technology", laptop.getCategory());
+  }
+
+  @Test
+  public void canGetType() {
+    assertEquals("Laptop", laptop.getType());
+  }
+
+  @Test
+  public void canGetPrice() {
+    assertEquals(800.00, laptop.getPrice(),0.1);
+  }
+
+  @Test
+  public void canGetModel() {
+    assertEquals("Macbook", laptop.getMake());
+  }
+
+  @Test
+  public void canGetMake() {
+    assertEquals("Pro", laptop.getModel());
+  }
+
+  @Test
+  public void canGetScreenSize() {
+    assertEquals(17, laptop.getScreenSize());
+  }
 }
