f5e45c12b65f7df88376037cce31d1d12ecb6a7f
Marilena Alexe <marilena.alexe@gmail.com>
2017-06-12 07:38:30 +0100

    Update <WashingMachine> & <WashingMachineTest> classes to count for the <Discountable> interface implementation.

--- models/WashingMachine.java
+++ models/WashingMachine.java
@@ -1,11 +1,12 @@
 package models;
-
 import java.util.*;
+import interfaces.*;
 
-public class WashingMachine extends Item {
+public class WashingMachine extends Item implements Discountable {
 
   private int programs;
   private int maxSpin;
+  private Discount discount;
 
   public WashingMachine(String category, String type, Double price, String make, String model, int programs, int maxSpin) {
     super(category, type, price, make, model);
@@ -19,5 +20,21 @@ public class WashingMachine extends Item {
 
   public int getMaxSpin() {
     return this.maxSpin;
+  }
+
+  public Discount setDiscount(Discount newDiscount) {
+    return this.discount = newDiscount;
+  }
+
+  public String getDiscountLabel() {
+    return discount.getLabel();
+  }
+
+  public String getDiscountType() {
+    return discount.getType();
+  }
+
+  public int getDiscountValue() {
+    return discount.getValue();
   }
 }
--- specs/WashingMachineTest.java
+++ specs/WashingMachineTest.java
@@ -11,10 +11,12 @@ import models.*;
 public class WashingMachineTest {
 
   WashingMachine washingMachine;
+  Discount discount;
 
   @Before
   public void before() {
     washingMachine = new WashingMachine("HomeElectrics","WashingMachine", 325.00, "Indesit", "xyz", 14, 1600);
+    discount = new Discount("Buy one get one free","percentage", 50);
   }
 
   @Test
@@ -35,6 +37,24 @@ public class WashingMachineTest {
   @Test
   public void canGetMaxSpin() {
     assertEquals(1600, washingMachine.getMaxSpin());
+  }
+
+  @Test
+  public void canGetDiscountLabel() {
+    washingMachine.setDiscount(discount);
+    assertEquals("Buy one get one free", washingMachine.getDiscountLabel());
+  }
+
+  @Test
+  public void canGetDiscountType() {
+    washingMachine.setDiscount(discount);
+    assertEquals("percentage", washingMachine.getDiscountType());
+  }
+
+  @Test
+  public void canGetDiscountValue() {
+    washingMachine.setDiscount(discount);
+    assertEquals(50, washingMachine.getDiscountValue());
   }
 
 }
